fd3da1add3ee4b60beb79312a7914ddea1544c57
qzLiuDongGuo <18365216935@163.com>
2016-08-25 19:38:04 +0800

    Update Test2.java
    gitHub update 02

--- src/com/git/test/Test2.java
+++ src/com/git/test/Test2.java
@@ -4,5 +4,6 @@ public class Test2 {
 	public static void main(String[] args) {
 		System.out.println("1111111111");
 		System.out.println("2222222222");
+		System.out.println("gitHub update 02");
 	}
 }
